2469b92147ab9c2f1bfdefcd564256a7e3fbed37
marcelogdiaz <marcelogdiaz@gmail.com>
2023-04-04 15:17:58 -0300

    remove comment

--- S4string/Program.cs
+++ S4string/Program.cs
@@ -81,11 +81,6 @@ namespace S4string
             Console.WriteLine($"He was first published in {jh.published} at the age of {jh.published - jh.born}.");
             Console.WriteLine($"He'd be over {Math.Round((2018d - jh.born) / 100d) * 100d} years old today.");
 
-            // Output:
-            // Jupiter Hammon was an African American poet born in 1711.
-            // He was first published in 1761 at the age of 50.
-            // He'd be over 300 years old today.
-
             //SUBCADENAS
             string s3 = "Visual C# Express";
             System.Console.WriteLine(s3.Substring(7, 2));
