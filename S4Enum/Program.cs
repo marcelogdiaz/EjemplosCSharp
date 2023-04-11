8fb277e70ee7295e52d01a1090db5266f4dd4972
marcelogdiaz <marcelogdiaz@gmail.com>
2023-04-11 18:17:20 -0300

    Cambios struct

---
+++ S4Enum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S4Enum
+{
+    internal class Program{
+        enum WeekDays
+                    {
+                        Monday,
+                        Tuesday,
+                        Wednesday,
+                        Thursday,
+                        Friday,
+                        Saturday,
+                        Sunday
+                    }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WeekDays.Friday); //output: Friday 
+
+            int day = (int)WeekDays.Friday; // enum to int conversion
+            Console.WriteLine(day); //output: 4 
+		
+            var wd = (WeekDays)5; // int to enum conversion
+            Console.WriteLine(wd);//output: Saturday 
+
+
+            var today = WeekDays.Tuesday;
+            if (today == WeekDays.Saturday || today == WeekDays.Sunday)
+            {
+                Console.WriteLine("It's the weekend!");
+            }
+            else
+            {
+                Console.WriteLine("It's a weekday.");
+            }
+
+            Console.WriteLine("Presione ENTER para salir");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ S4Struct/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S4Struct
+{
+    internal class Program
+    {
+        struct Coordinate
+        {
+            public int x;
+            public int y;
+
+            public Coordinate(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public static Coordinate GetOrigin()
+            {
+                return new Coordinate();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Coordinate point = new Coordinate(10, 20);
+
+            Console.WriteLine("X:" + point.x); //output: 10  
+            Console.WriteLine("Y:" + point.y); //output: 20
+
+
+            point = Coordinate.GetOrigin();            
+
+            Console.WriteLine("X:" + point.x); //output: 0  
+            Console.WriteLine("Y:" + point.y); //output: 0  
+
+            Console.WriteLine("Presione ENTER para salir");
+            Console.ReadLine();
+        }
+    }
+}
